8e5ecdeb80933810728da262a77b98132f389fc8
John <jhall91@hotmail.com>
2024-05-05 18:22:31 +0100

    try 5

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -9,6 +9,7 @@ from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
 
+
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
     """Test commands."""
@@ -16,7 +17,9 @@ class CommandTests(SimpleTestCase):
     def test_wait_for_db_ready(self, patched_check):
         """Test waiting for database if database ready."""
         patched_check.return_value = True
+
         call_command('wait_for_db')
+
         patched_check.assert_called_once_with(databases=['default'])
 
     @patch('time.sleep')
@@ -24,6 +27,8 @@ class CommandTests(SimpleTestCase):
         """Test waiting for database when getting OperationalError."""
         patched_check.side_effect = [Psycopg2OpError] * 2 + \
             [OperationalError] * 3 + [True]
+
         call_command('wait_for_db')
+
         self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
